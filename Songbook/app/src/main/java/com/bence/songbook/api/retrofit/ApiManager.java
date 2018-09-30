1720b4b78a6f9cbde2bf5aa541cd18bc233c2228
Bako Bence <bakobence@yahoo.com>
2018-09-30 14:47:56 +0300

    Setting SECOND BASE_URL

--- Songbook/app/src/main/java/com/bence/songbook/api/retrofit/ApiManager.java
+++ Songbook/app/src/main/java/com/bence/songbook/api/retrofit/ApiManager.java
@@ -1,14 +1,18 @@
 package com.bence.songbook.api.retrofit;
 
+import com.bence.projector.common.dto.LanguageDTO;
 import com.bence.projector.common.serializer.DateDeserializer;
 import com.bence.projector.common.serializer.DateSerializer;
 import com.google.gson.GsonBuilder;
 
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -16,20 +20,42 @@ public class ApiManager {
 
     public static Retrofit getClient() {
         String BASE_URL = "http://192.168.100.4:80";
+        String SECOND_BASE_URL = "http://192.168.100.5:80";
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
         httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient shortOkHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(3, TimeUnit.SECONDS)
+                .readTimeout(100, TimeUnit.SECONDS)
+                .addInterceptor(httpLoggingInterceptor).build();
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .connectTimeout(100, TimeUnit.SECONDS)
                 .readTimeout(100, TimeUnit.SECONDS)
                 .addInterceptor(httpLoggingInterceptor).build();
+
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.registerTypeAdapter(Date.class, new DateDeserializer()).registerTypeAdapter(Date.class,
                 new DateSerializer());
         GsonConverterFactory factory = GsonConverterFactory.create(gsonBuilder.create());
-        return new Retrofit.Builder()
+        Retrofit build = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .addConverterFactory(factory)
+                .client(shortOkHttpClient)
+                .build();
+        Call<List<LanguageDTO>> languages = build.create(LanguageApi.class).getLanguages();
+        Response<List<LanguageDTO>> response;
+        try {
+            response = languages.execute();
+            if (response.isSuccessful()) {
+                return build;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new Retrofit.Builder()
+                .baseUrl(SECOND_BASE_URL)
                 .addConverterFactory(factory)
                 .client(okHttpClient)
                 .build();
+
     }
 }
