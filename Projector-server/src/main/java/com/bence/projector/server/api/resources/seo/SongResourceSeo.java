e816acec9812418e50966856e2e6ace9a4e3b51b
Bako Bence <bakobence@yahoo.com>
2018-10-08 12:19:02 +0300

    Youtube og image changed

--- Projector-server/src/main/java/com/bence/projector/server/api/resources/seo/SongResourceSeo.java
+++ Projector-server/src/main/java/com/bence/projector/server/api/resources/seo/SongResourceSeo.java
@@ -86,7 +86,7 @@ public class SongResourceSeo {
         keywords.append(", ").append(song.getTitle().replaceAll(regex, ""));
 
         model.addAttribute("keywords", keywords.toString());
-        model.addAttribute("youtubeUrl", "http://img.youtube.com/vi/" + song.getYoutubeUrl() + "/sddefault.jpg");
+        model.addAttribute("youtubeUrl", "http://img.youtube.com/vi/" + song.getYoutubeUrl() + "/0.jpg");
         return "song";
     }
 }
