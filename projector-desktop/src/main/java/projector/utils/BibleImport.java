e1a56f363a04d0f553cda54c1d617acc4b3d3169
Bako Bence <bakobence@yahoo.com>
2019-10-15 19:53:16 +0300

    Bible import improvements

--- projector-desktop/src/main/java/projector/utils/BibleImport.java
+++ projector-desktop/src/main/java/projector/utils/BibleImport.java
@@ -1,3 +1,4 @@
+
 package projector.utils;
 
 import com.google.gson.Gson;
@@ -32,6 +33,183 @@ import java.util.List;
 public class BibleImport {
 
     private static Settings settings = Settings.getInstance();
+
+    public static void main(String[] args) {
+        //bibleImportFromJson();
+    }
+
+    public static void bibleImporting() {
+        List<Bible> bibles = ServiceManager.getBibleService().findAll();
+        Bible bible = bibles.get(3);
+        setIndicesForBible(bible);
+        //bibleImportFromJson();
+    }
+
+    public static void bibleImportFromJson() {
+        FileInputStream inputStream;
+        try {
+            inputStream = new FileInputStream("booktitle.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            String line = br.readLine();
+            List<String> titles = new ArrayList<>();
+            while (line != null) {
+                titles.add(line.trim());
+                line = br.readLine();
+            }
+            br.close();
+            inputStream = new FileInputStream("bible.txt");
+            br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            StringBuilder s = new StringBuilder();
+            List<String> verses = new ArrayList<>();
+            line = br.readLine();
+            List<Book> books = new ArrayList<>();
+            int previousBookNr = 0;
+            int previousPartNr = 0;
+            int bookNr;
+            int partNr;
+            Book book = null;
+            Chapter chapter = null;
+            while (line != null) {
+                String[] split = line.split("\\|\\|");
+                bookNr = Integer.parseInt(split[0].replaceAll("[^0-9]", ""));
+                partNr = Integer.parseInt(split[1]);
+                int verseNr = Integer.parseInt(split[2]);
+                boolean newBook = bookNr != previousBookNr;
+                if (newBook) {
+                    previousBookNr = bookNr;
+                    book = new Book();
+                    book.setTitle(titles.get(bookNr - 1));
+                    books.add(book);
+                }
+                if (newBook || partNr != previousPartNr) {
+                    previousPartNr = partNr;
+                    chapter = new Chapter();
+                    chapter.setBook(book);
+                    chapter.setNumber((short) partNr);
+                    List<Chapter> chapters = book.getChapters();
+                    if (chapters == null) {
+                        chapters = new ArrayList<>();
+                        book.setChapters(chapters);
+                    }
+                    chapters.add(chapter);
+                }
+                List<BibleVerse> bibleVerses = chapter.getVerses();
+                if (bibleVerses == null) {
+                    bibleVerses = new ArrayList<>();
+                    chapter.setVerses(bibleVerses);
+                }
+                BibleVerse verse = new BibleVerse();
+                bibleVerses.add(verse);
+                verse.setNumber((short) verseNr);
+                verse.setText(split[3].trim());
+                verse.setChapter(chapter);
+                s.append(line);
+                line = br.readLine();
+            }
+            //Gson gson = new GsonBuilder().serializeNulls().create();
+            //ArrayList<BibleBook> bookArrayList;
+            //Type listType = new TypeToken<ArrayList<BibleBook>>() {
+            //}.getType();
+            //bookArrayList = gson.fromJson(s.toString(), listType);
+            //char[] chars = s.toString().toCharArray();
+            //boolean quote = false;
+            //boolean isBook = false;
+            //boolean isChapter = false;
+            //boolean isVerse = false;
+            //boolean isScripture = false;
+            //boolean isValue = false;
+            //boolean isEnd = false;
+            //String value = "";
+            //int book;
+            //int chapter;
+            //int verse;
+            //int x;
+            //String scripture = "";
+            //for (char c : chars) {
+            //    if (quote) {
+            //        if (c == 'B') {
+            //            isBook = true;
+            //        } else if (c == 'C') {
+            //            isChapter = true;
+            //        } else if (c == 'V') {
+            //            isVerse = true;
+            //        } else if (c == 'S') {
+            //            isScripture = true;
+            //        }
+            //    } else if (isValue) {
+            //        if (!(c >= '0' && c <= '9')) {
+            //            isValue = false;
+            //            x = Integer.parseInt(value);
+            //            if (isBook) {
+            //                book = x;
+            //                isBook = false;
+            //            } else if (isChapter) {
+            //                chapter = x;
+            //                isChapter = false;
+            //            } else if (isVerse) {
+            //                verse = x;
+            //                isVerse = false;
+            //            } else if (isScripture) {
+            //                scripture += getCharFromX(x);
+            //            }
+            //            value = "";
+            //        }
+            //    }
+            //
+            //    if (c == '[') {
+            //
+            //    } else if (c == '{') {
+            //
+            //    } else if (c == '}') {
+            //        if (!scripture.isEmpty()) {
+            //            verses.add(scripture);
+            //            scripture = "";
+            //            isScripture = false;
+            //        }
+            //    } else if (c == ',') {
+            //
+            //    } else if (c == ' ' || c == ':' || c == ',') {
+            //    } else if (c == '"') {
+            //        quote = !quote;
+            //    } else if (c >= '0' && c <= '9') {
+            //        value += c;
+            //        isValue = true;
+            //    }
+            //}
+            System.out.println(books.size());
+
+            Bible bible = new Bible();
+            bible.setBooks(books);
+            List<Language> languages = ServiceManager.getLanguageService().findAll();
+            bible.setLanguage(languages.get(4));
+            bible.setCreatedDate(new Date());
+            bible.setModifiedDate(bible.getCreatedDate());
+            bible.setName("TLAB Ang Biblia");
+            bible.setShortName("TLAB");
+            createIndices(bible);
+            //ServiceManager.getBibleService().create(bible);
+            setIndicesForBible(bible);
+
+            //verseImport(bible);
+        } catch (IOException ignored) {
+        }
+    }
+
+    private static char getCharFromX(int x) {
+        switch (x) {
+            case 81:
+                return 'N';
+            case 127:
+                return 'g';
+            case 129:
+                return 'a';
+            case 163:
+                return 'n';
+            case 7:
+                return ' ';
+        }
+        return '?';
+    }
 
     public static void bibleImport() {
         FileInputStream inputStream;
