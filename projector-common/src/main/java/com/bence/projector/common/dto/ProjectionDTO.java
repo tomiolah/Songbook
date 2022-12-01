ee2f189c8bf2fa7623cfb9a18ed539da4f534ca9
Bako Bencze <bence@gineri.com>
2022-12-01 22:49:54 +0200

    #38a6p2t Expose

--- projector-common/src/main/java/com/bence/projector/common/dto/ProjectionDTO.java
+++ projector-common/src/main/java/com/bence/projector/common/dto/ProjectionDTO.java
@@ -1,8 +1,12 @@
 package com.bence.projector.common.dto;
 
+import com.google.gson.annotations.Expose;
+
 import java.util.List;
 
 public class ProjectionDTO {
+
+    @Expose
     private List<Long> verseIndices;
 
     public List<Long> getVerseIndices() {
