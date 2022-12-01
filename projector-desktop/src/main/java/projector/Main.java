8974afa5a6988133b8c6b455a7073110510991ce
Bako Bencze <bence@gineri.com>
2022-12-01 22:44:01 +0200

    preferIPv4Stack

--- projector-desktop/src/main/java/projector/Main.java
+++ projector-desktop/src/main/java/projector/Main.java
@@ -3,6 +3,11 @@ package projector;
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            System.setProperty("java.net.preferIPv4Stack", "true");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         MainDesktop.main(args);
     }
 }
