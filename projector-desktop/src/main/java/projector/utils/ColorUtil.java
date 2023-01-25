19aeabaea39e54626384421082a1ddffa3a28656
Bako Bencze <bence@gineri.com>
2023-01-25 18:25:55 +0200

    #2yux9gv Better dark theme

---
+++ projector-desktop/src/main/java/projector/utils/ColorUtil.java
@@ -0,0 +1,22 @@
+package projector.utils;
+
+import javafx.scene.paint.Color;
+
+public class ColorUtil {
+
+    public static Color getCollectionNameColor() {
+        return Color.rgb(224, 247, 250);
+    }
+
+    public static Color getSongTitleColor() {
+        return getGeneralTextColor();
+    }
+
+    public static Color getGeneralTextColor() {
+        return Color.rgb(255, 255, 255);
+    }
+
+    public static Color getReferenceTextColor() {
+        return Color.rgb(200, 224, 255);
+    }
+}
