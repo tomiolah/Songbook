7c52cf72a9f62ed73ec6b207a4f8b0b11a2556cf
Bako Bencze <bence@gineri.com>
2023-06-10 23:57:04 +0300

    Added non capitalized words

--- Projector-server/src/main/java/com/bence/projector/server/utils/UserUtil.java
+++ Projector-server/src/main/java/com/bence/projector/server/utils/UserUtil.java
@@ -8,12 +8,16 @@ import com.bence.projector.server.backend.service.UserService;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unused")
 public class UserUtil {
 
     public static void markHadUploadedSongsForUsers(UserService userService, SongRepository songRepository) {
         List<User> users = userService.findAll();
         List<User> modifiedUsers = new ArrayList<>();
         for (User user : users) {
+            if (user.isHadUploadedSongs()) {
+                continue;
+            }
             List<Song> allByCreatedByEmail = songRepository.findAllByCreatedByEmail(user.getEmail());
             user.setHadUploadedSongs(allByCreatedByEmail != null && allByCreatedByEmail.size() > 0);
             if (user.isHadUploadedSongs()) {
